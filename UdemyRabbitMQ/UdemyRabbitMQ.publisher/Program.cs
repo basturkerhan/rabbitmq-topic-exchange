153b16b357c93a09b8dc6fbd4710ef3e83d406c3
Erhan Basturk <56194980+basturkerhan@users.noreply.github.com>
2022-09-26 14:00:10 +0300

    Initial commit

---
+++ UdemyRabbitMQ/UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4,
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs-topic", durable:true, type:ExchangeType.Topic);
+            Random random = new Random();
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log1 = (LogNames)random.Next(1, 5);
+                LogNames log2 = (LogNames)random.Next(1, 5);
+                LogNames log3 = (LogNames)random.Next(1, 5);
+                var routeKey = $"{log1}.{log2}.{log3}";
+
+                string message = $"Log: {x} and Type: {log1}-{log2}-{log3}";
+                var messageBody = Encoding.UTF8.GetBytes(message); // kuyruğa bayt olarak gönderilir
+
+                channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+                Console.WriteLine($"Log Gönderildi: {message}");
+            });
+  
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ/UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber // consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Kuyruk oluşturma
+            var queueName = channel.QueueDeclare().QueueName;
+            var routeKey = "*.Error.*";
+            channel.QueueBind(queueName, "logs-topic", routeKey);
+            channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine("Loglar Dinleniyor...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(500);
+                Console.WriteLine("Kuyruktan Gelen Mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false); // mesajı artık kuyruktan silebilirsin diye belirttik (yukarıyı false yaptığımız için)
+            };
+
+            Console.ReadLine();
+        }
+
+    }
+}
